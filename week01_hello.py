fab0cfe84c5b6cd6115617c7fe63b2495d8f0521
tarnished <trnshd24@gmail.com>
2024-03-12 20:31:23 +0900

    v0.4 prime number

--- week01_hello.py
+++ week01_hello.py
@@ -1,3 +1,20 @@
 # 실행: alt + shift + f10
-name = input("이름 입력 : ")
-print(f"{name}님 환영합니다!")
+#1번
+#name = input("name input : ")
+#print(f"welcome {name}")
+
+number = int(input("Input number"))
+
+if number < 2:
+   count = 1
+else:
+    count = 0
+    for i in range(1, number+1):
+        if number % i == 0:
+           count = count + 1
+
+
+if count == 2:
+    print(f"{number} is prime number")
+else:
+    print(f"{number} is not prime number")
