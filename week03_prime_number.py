681e05ba227eaa9cafc403f0d8fecc1213b2f5a8
tarnished <trnshd24@gmail.com>
2024-03-19 20:28:19 +0900

    v1.4 prime_number

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,16 +1,16 @@
+def is_prime_number(n) -> bool:
+    """
+    prime number determination function
+    :param n: positive integer
+    :return: Returns true if prime, otherwise returns False.
+    """
+    pass
+
+
 
 start, end = list(map(int, input("Input number : ").split()))
 
 
 for k in range(start, end+1):
-    is_prime_number = True
-    if k < 2:
-        is_prime_number = False
-    else:
-        i = 2
-        while i*i <= k:
-            if k % i == 0:
-                is_prime_number = False
-                break
-            i = i + 1
+
         if is_prime_number: print(k, end=' ')
