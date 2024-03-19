892097c58e287998ec522094fb5458b996480875
tarnished <trnshd24@gmail.com>
2024-03-19 19:46:31 +0900

    ãv1.2 prime_number

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-This is a remote repository for the “Artificial Intelligence-based Information Science Exploration” class held at Incheon Science High School in the first semester of the 2024 school year. This repsitory uploads and manages code and practice examples of programs studied in class.:
+This is a remote repository for the 
+“Artificial Intelligence-based Information Science Exploration” 
+class held at Incheon Science High School 
+in the first semester of the 2024 school year. 
+This repsitory uploads and manages code and practice examples of programs
+studied in class.:
--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,17 +1,18 @@
-start = int(input("Input initial number : "))
-end = int(input("Input final number : "))
+test = input("Input start number : ").split()
+#print(int(test[0], int(test[1])))
+
+start = int(test[0])
+end = int(test[1])
 
 for k in range(start, end+1):
-       is_prime_number = True
-       if k < 2:
-            is_prime_number = False
-       else:
-           i = 2
-           while i*i <= k:
-               if k % i == 0:
-                    is_prime_number = False
-                    break
-                i = i + 1
-            if is_prime_number: print(k, end=' ')
-
-
+    is_prime_number = True
+    if k < 2:
+        is_prime_number = False
+    else:
+        i = 2
+        while i*i <= k:
+            if k % i == 0:
+                is_prime_number = False
+                break
+            i = i + 1
+        if is_prime_number: print(k, end=' ')
