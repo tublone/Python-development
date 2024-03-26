93051e28efee199b67f5f2608f570829a99b21b8
tarnished <trnshd24@gmail.com>
2024-03-26 20:40:35 +0900

    v0.1 math

--- week04_mathematics.py
+++ week04_mathematics.py
@@ -33,19 +33,19 @@ def power(b, e) -> int:
 
 
 while True:
-        menu = int(input("1. prime number   2. power   3. divmod   4. quit : ")) # ValueError: invalid literal for int() with base 10: '박준영'
-        if menu == 1:
+        menu = (input("1. prime number   2. power   3. divmod   4. quit : ")) # ValueError: invalid literal for int() with base 10: '박준영'
+        if menu == '1':
             start, end = list(map(int, input("Input start & end number : ").split()))
             for k in range(start, end + 1):
                 if is_prime_number(k): print(k, end=' ')
-        elif menu == 2:
+        elif menu == '2':
             base, exponent = map(int, input("Input base & exponent number : ").split())
             print(f"{base}^{exponent} = {power(base, exponent)}")
-        elif menu == 3:
+        elif menu == '3':
             dividend, divisor = map(int, input("Input dividend & divisor number : ").split())
             print(f" {dividend} // {divisor} = {divmod(dividend, divisor)[0]}")
             print(f" {dividend} % {divisor} = {divmod(dividend, divisor)[1]}")
-        elif menu == 4:
+        elif menu == '4':
             print("exit program.")
             break  #exit()
         else:
