f7208dfc431dd18e3cc1a275cda34508631001b8
tarnished <trnshd24@gmail.com>
2024-03-26 19:11:19 +0900

    v0.1 power

--- week04_power.py
+++ week04_power.py
@@ -1,2 +1,3 @@
 base, exponent = map(int, input("Input base & exponent number : ").split())
-print(f"{base}^{exponent} = {base**exponent}" )
+print(f"{base}^{exponent} = {base**exponent}" )  # operator
+print(f"{base}^{exponent} = {pow(base, exponent)}" ) # built in function
