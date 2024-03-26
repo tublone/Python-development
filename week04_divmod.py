a3d3553ef35e79c299000812a5812b1cda39dfee
tarnished <trnshd24@gmail.com>
2024-03-26 19:44:32 +0900

    v0.0 divmod

---
+++ week04_divmod.py
@@ -0,0 +1,5 @@
+dividend, divisor = map(int, input("Input dividend & divisor number : ").split())
+print(f" {dividend} // {divisor} = {dividend // divisor}")
+print(f" {dividend} % {divisor} = {dividend % divisor}")
+print(f" {dividend} // {divisor} = {divmod(dividend, divisor)[0]}")
+print(f" {dividend} % {divisor} = {divmod(dividend, divisor)[1]}")
