087554732e0d4c870d35dab1f0c0168400b2814d
June <trnshd24@gmail.com>
2024-04-02 20:30:44 +0900

    v0.2 kiosk

--- week05_kiosk.py
+++ week05_kiosk.py
@@ -3,13 +3,20 @@
 
 beverage = ["Black Coffee", "Latte"]
 prices = [1500, 2500]
+total_price = 0
 
 while True:
-    menu = int(input("1) Black Coffee  2) Cafe Latte  3) End Order : "))
-    if menu == 3:
-        print("Exit Program")
+    menu = (input("1) Black Coffee  2) Cafe Latte  3) End Order : "))
+    if menu == '3':
+        print("Your order has been accepted.")
         break
-    elif menu == 1:
+    elif menu == '1':
         print("You ordered Black Coffee. The price is 1500 won.")
-    elif menu == 2:
+        total_price = total_price + prices[0]
+    elif menu == '2':
         print("You ordered Cafe Latte. The price is 2500 won.")
+        total_price = total_price + prices[1]
+    else:
+        print(f"Menu number {menu} you ordered does not exist. Please choose from the menu")
+
+print(f"Your order has been accepted. The total amount is {total_price} won.")
